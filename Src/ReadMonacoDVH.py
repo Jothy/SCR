e93b203dcd9fbe873c80a660cda4a7fda63cf6ab
Jothy <Jothybasu@gmail.com>
2017-08-03 20:02:49 +1000

    Functionality to convert cumDVHs to DiffDVHs and pack to JSONs

--- Src/ReadMonacoDVH.py
+++ Src/ReadMonacoDVH.py
@@ -36,8 +36,47 @@ def ReadDVHDataMonaco(fp):
     return dvhInfo
 
 
+
+def ReadTomoDVHFromXLS(fp):
+    dvhInfo = {}
+    dvhInfo['PatID'] ='12345'
+    dvhInfo['PlanName'] = 'CSI'
+    dvhInfo['DoseUnits'] ='Gy'
+    dvhInfo['VolumeUnits'] ='cc'
+    curFile = xlrd.open_workbook(fp)
+    curSheet=curFile.sheet_by_index(0)
+    # Read the dose column first
+    DoseCol = curSheet.col_values(0)
+    DoseCol = DoseCol[2:np.size(DoseCol)]
+    [float(i) for i in DoseCol]
+
+    for x in range(1, curSheet.ncols, 1):
+        curDVH = {}
+        ROINum = x
+        ROI = curSheet.col_values(ROINum)
+        ROIName = ROI[1].split(sep='(')[0]
+        #print(ROIName)
+        TotalVol = np.float(ROI[1].split(sep='(')[2].split(sep=':')[1].split(sep=')')[0])
+        # print(TotalVol)
+        VolCol = ROI[2:np.size(ROI)]
+        while '' in VolCol:
+            VolCol.remove('')
+        [float(i) for i in VolCol]
+        # Convert cumulative DVH to differential DVH
+        DiffVols = []
+        DiffDoses = []
+        for y in range(0, np.size(VolCol) - 1, 1):
+            DiffVols.append(((VolCol[y] - VolCol[y + 1]) * TotalVol)/100.0)
+            DiffDoses.append((DoseCol[y] + DoseCol[y + 1])/2.0)
+
+        curDVH['DoseBins'] =DiffDoses
+        curDVH['VolBins'] = DiffVols
+        dvhInfo[str(ROIName)] = curDVH
+    return dvhInfo
+
+
 def WriteToJSON(data,fp):
-    filepath=fp+'\\'+str(data['PatID'])+'_VMAT'+'.json'
+    filepath=fp+'\\'+str(data['PatID'])+'_Tomo'+'.json'
     with open(filepath,'w') as OutFile:
         js.dump(data, OutFile)
         #print(filepath,':Written')
@@ -50,38 +89,35 @@ def JSONtoDict(JSONfile):
     return DVHData
 
 
+
+fp='D:\\Projects\\SCR\\Data\\CSI5_Tomo.xlsx'
+Data=ReadTomoDVHFromXLS(fp)
+WriteToJSON(Data,'D:\\Projects\\SCR\\Data')
+
+
+
+
+
+
+
 # fp="D:\Projects\SCR\Data\CSI10\CSI10_VMAT_DVH_1.txt"
 # fw="D:\Projects\SCR\Data\CSI10"
 # data=ReadDVHDataMonaco(fp)
 # WriteToJSON(data,fw)
 
-curFile=xlrd.open_workbook('D:\Projects\SCR\Data\CSI5_Tomo.xlsx')
-curSheet=curFile.sheet_by_name('CSI5_Tomo')
-
-#Read the dose column first
-DoseCol=curSheet.col_values(0)
-DoseCol=DoseCol[2:np.size(DoseCol)]
-[float(i) for i in DoseCol]
-print(curSheet.ncols,":Cols")
-
-ROINum=7
-ROI=curSheet.col_values(ROINum)
-ROIName=ROI[1].split(sep='(')[0]
-print(ROIName)
-TotalVol=np.float(ROI[1].split(sep='(')[2].split(sep=':')[1].split(sep=')')[0])
-print(TotalVol)
-VolCol=ROI[2:np.size(ROI)]
-while '' in VolCol:
-    VolCol.remove('')
-[float(i) for i in VolCol]
+# #plot diff DVH for testing
+# pl.plot(DiffDoses1,DiffVols,linewidth=3.0)
+# pl.xlim(0)
+# pl.ylim(0)
+# pl.show()
 
 
-print(ROIName,TotalVol)
-pl.plot(DoseCol[0:np.size(VolCol)],VolCol,linewidth=3.0)
-pl.xlim(0)
-pl.ylim(0,100)
-pl.grid(True)
-pl.show()
+# print(ROIName,TotalVol)
+# pl.plot(DoseCol[0:np.size(VolCol)],VolCol,linewidth=3.0)
+# pl.xlim(0)
+# pl.ylim(0,100)
+# pl.grid(True)
+# pl.show()
 
 
 
